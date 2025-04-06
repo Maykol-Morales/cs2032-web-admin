b79c9ab60bec765500cb6161a383c62bbac5c689
Maykol Morales Morante <maykol.morales.morante@gmail.com>
2025-04-06 17:42:29 -0500

    test

--- src/hooks/use-attendance.ts
+++ src/hooks/use-attendance.ts
@@ -21,6 +21,9 @@ type LocationData = {
 
 type AttendanceStatus = "idle" | "loading" | "success" | "error"
 
+const url = import.meta.env.PUBLIC_BACK_END_URL! as string;
+const key = import.meta.env.PUBLIC_BACK_END_KEY! as string;
+
 export function useAttendance() {
     const [ status, setStatus ] = useState<AttendanceStatus>("idle")
 
@@ -29,77 +32,77 @@ export function useAttendance() {
 
         setStatus("loading")
 
-        const test = await fetch("https://api.ipify.org/?format=json", {
-            method: "GET"
-        })
-
-        const res = await test.json();
-        console.log(res);
-
-        const url = import.meta.env.PUBLIC_BACK_END_URL! as string;
-        const key = import.meta.env.PUBLIC_BACK_END_KEY! as string
-
         try {
             const response = await fetch(url, {
-                method: "POST",
+                method: "GET",
                 headers: {
-                    "Content-Type": "application/json",
-                    "X-Internal-Secret": key,
-                },
-                body: JSON.stringify({
-                    course_id: parameter.course,
-                    session_id: parameter.session,
-                    student_email: user.email,
-                    student_latitude: location.latitude,
-                    student_longitude: location.longitude,
-                }),
+                    "x-api-key": key,
+                }
             })
 
-            const status = response.status;
             const json = await response.json();
-
             console.log(json);
 
-            switch (status) {
-                case 400:
-                    setStatus("error")
-                    toast.error("No se pudo registrar la asistencia.", {
-                        description: "Tu correo electrónico no es de UTEC."
-                    })
-                    break;
-                case 401:
-                    setStatus("error")
-                    toast.error("No se pudo registrar la asistencia.", {
-                        description: "Sesión no encontrada."
-                    })
-                    break;
-                case 402:
-                    setStatus("error")
-                    toast.error("No se pudo registrar la asistencia.", {
-                        description: "Asistencia ya registrada."
-                    })
-                    break;
-                case 403:
-                    setStatus("error")
-                    toast.error("No se pudo registrar la asistencia.", {
-                        description: "Sesión expirada."
-                    })
-                    break;
-                case 404:
-                    setStatus("error")
-                    toast.error("No se pudo registrar la asistencia.", {
-                        description: "No estás en UTEC."
-                    })
-                    break;
-
-                case 200:
-                    setStatus("success")
-                    toast.success("Asistencia registrada con éxito.")
-                    break;
-                default:
-                    break;
-
-            }
+            // const response = await fetch(url, {
+            //     method: "POST",
+            //     headers: {
+            //         "Content-Type": "application/json",
+            //         "X-Internal-Secret": key,
+            //     },
+            //     body: JSON.stringify({
+            //         course_id: parameter.course,
+            //         session_id: parameter.session,
+            //         student_email: user.email,
+            //         student_latitude: location.latitude,
+            //         student_longitude: location.longitude,
+            //     }),
+            // })
+            //
+            // const status = response.status;
+            // const json = await response.json();
+            //
+            // console.log(json);
+            //
+            // switch (status) {
+            //     case 400:
+            //         setStatus("error")
+            //         toast.error("No se pudo registrar la asistencia.", {
+            //             description: "Tu correo electrónico no es de UTEC."
+            //         })
+            //         break;
+            //     case 401:
+            //         setStatus("error")
+            //         toast.error("No se pudo registrar la asistencia.", {
+            //             description: "Sesión no encontrada."
+            //         })
+            //         break;
+            //     case 402:
+            //         setStatus("error")
+            //         toast.error("No se pudo registrar la asistencia.", {
+            //             description: "Asistencia ya registrada."
+            //         })
+            //         break;
+            //     case 403:
+            //         setStatus("error")
+            //         toast.error("No se pudo registrar la asistencia.", {
+            //             description: "Sesión expirada."
+            //         })
+            //         break;
+            //     case 404:
+            //         setStatus("error")
+            //         toast.error("No se pudo registrar la asistencia.", {
+            //             description: "No estás en UTEC."
+            //         })
+            //         break;
+            //
+            //     case 200:
+            //         setStatus("success")
+            //         toast.success("Asistencia registrada con éxito.")
+            //         break;
+            //     default:
+            //         break;
+            //
+            // }
         } catch (error) {
             setStatus("error")
             console.log(error)
